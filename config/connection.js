e5b9598a38724c9f1eaaaf96551e14440f5452a0
tomkent321 <tomkent321@yahoo.com>
2018-10-01 14:45:40 -0600

    config
    config

--- config/connection.js
+++ config/connection.js
@@ -8,11 +8,11 @@ var mysql = require("mysql");
 //   password: "root",
 //   database: "burgers_db"
 // });
-
+var connection;
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else {
-  var connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
